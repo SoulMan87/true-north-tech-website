4668befce8491d33b31bbd379716f46da3b84672
jonathan <jonathanhinestroza87@gmail.com>
2026-04-30 17:51:49 -0500

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes, HashRouter as Router } from 'react-router-dom'; // ← Solo cambia esta línea
 import ScrollToTop from './components/ScrollToTop';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
